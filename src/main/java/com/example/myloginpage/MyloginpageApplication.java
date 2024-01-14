b8dafe09a792728bd00a6772c82005300fe6def9
Ranjith <ranjith100805@gmail.com>
2024-01-14 19:59:03 +0530

    My First Project

---
+++ src/main/java/com/example/myloginpage/MyloginpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.myloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloginpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloginpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myloginpage/MyloginpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloginpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
